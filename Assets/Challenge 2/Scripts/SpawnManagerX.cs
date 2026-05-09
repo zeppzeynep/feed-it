8e05d7dece688df433f07ab6a9a8868c8d2dec6b
zeppzeynep <zeynepaydin2003.55@gmail.com>
2026-05-09 16:51:17 +0300

    total

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnInterval = Random.Range(2.0f, 5.0f);
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int randomIndex = Random.Range(0,ballPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[randomIndex], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+
+}
--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -13,10 +13,10 @@ public class DestroyOutOfBounds : MonoBehaviour
     void Update()
     {
         //if an object (animal or bound) goes past the player view in the game, remove that object
-        if (transform.position.z > topBound)
+        if (transform.position.y > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        } else if (transform.position.y < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
